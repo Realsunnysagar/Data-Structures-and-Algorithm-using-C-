da3c50a556192f96bfd0e77f2b3d289b137d4782
Realsunnysagar <realsunnysagar@gmail.com>
2026-05-02 20:39:35 +0530

    keypad

---
+++ KeypadPhoneQues.cpp
@@ -0,0 +1,26 @@
+/*
+        abc  def
+    1   2   3
+   ghi jkl mno
+    4   5   6
+   pqrs tuv wxyz
+    7   8   9
+        0
+    String str "2 3" -->Combinations that we can form from char 2 and 3 i.e abc, def
+      | ab, bd, cd
+      | de, be, ce      -->ans
+      | aj, bj, cj
+    There will be some supporting we need to think about
+HW Ques
+    -a,b,c, .  . . . .  . z
+     1,2,3, .  . . ..  . .26
+     String str = "1 1 2"
+     string = aab, kb,aL --->ans
+     1127 --> 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+return 0; 
+}
